7064860b3caaaae1e2a126525c75db07c05453a2
Akyla <akylaaquino@hotmail.com>
2024-12-17 16:48:12 -0300

    feat: creating sql query and notebooks

---
+++ notebooks/queries/group_data.sql
@@ -0,0 +1,10 @@
+SELECT
+    a.userID,
+    a.surveyID,
+    a.answerText,
+    q.questionText,
+FROM answer a
+INNER JOIN question q
+ON a.questionID = q.questionID
+INNER JOIN survey s
+ON a.surveyID = s.surveyID;
